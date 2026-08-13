94642a5bb11d705f93af2df99704c549147526c7
farrokhzad743-dot <farrokhzad743@gmail.com>
2026-08-13 17:07:29 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 window.SITE_CONFIG = {
   siteUrl: 'https://tavonikohenor.ir',
+
   supabaseUrl: 'https://rktnuurdczwssyvjtglf.supabase.co',
+
   supabaseAnonKey: 'sb_publishable_FQecect95O_2VgnYIq21rg_RJY72OFX',
-  ownerEmails: ['farrokhzad743@gmail.com']
+
+  ownerEmails: [
+    'farrokhzad743@gmail.com'
+  ]
 };
