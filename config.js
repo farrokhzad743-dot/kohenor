31c87f88c5b89ee017e7b04e5745f821ccb3109c
farrokhzad743-dot <farrokhzad743@gmail.com>
2026-08-15 15:15:54 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.SITE_CONFIG = {
-  siteUrl: window.location.origin,
+  siteUrl: 'https://farrokhzad743-dot.github.io/kohenor/',
   supabaseUrl: 'https://rktnuurdczwssyvjtglf.supabase.co',
-  supabaseAnonKey: 'sb_publishable_FQecect95O_2VgnYIq21rg_RJY72OFX'
-,
+  // این کلید publishable/anon است و قرار نیست مخفی باشد. Service Role Key را هرگز اینجا نگذار.
+  supabaseAnonKey: 'sb_publishable_FQecect95O_2VgnYIq21rg_RJY72OFX',
   ownerEmails: [
     'farrokhzad743@gmail.com',
     'mrparvareh8@gmail.com'
