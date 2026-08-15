8e080eeb0866a636ba16410f0a424bfc4cbfc969
farrokhzad743-dot <farrokhzad743@gmail.com>
2026-08-15 14:03:10 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,10 @@
 window.SITE_CONFIG = {
-  // آدرس سایت
-  siteUrl: 'https://tavonikohenor.ir',
-
-  // آدرس پروژه Supabase
+  siteUrl: window.location.origin,
   supabaseUrl: 'https://rktnuurdczwssyvjtglf.supabase.co',
-
-  // فقط Anon / Publishable Key
-  // اینجا کلید عمومی Supabase را قرار بده
-  supabaseAnonKey: 'sb_publishable_FQecect95O_2VgnYIq21rg_RJY72OFX',
-
-  // ایمیل مالک سایت
+  supabaseAnonKey: 'sb_publishable_FQecect95O_2VgnYIq21rg_RJY72OFX'
+,
   ownerEmails: [
-    'farrokhzad743@gmail.com'
+    'farrokhzad743@gmail.com',
+    'mrparvareh8@gmail.com'
   ]
 };
